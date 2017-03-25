5de8295a9e4c02dca01d9ce83f34e05b437f34ad
fioricino <sarektoren@hotmail.com>
2017-03-25 23:41:38 +0300

    Ready to start

--- DBSyncTest/UnitTest1.cs
+++ DBSyncTest/UnitTest1.cs
@@ -33,5 +33,44 @@ namespace DBSyncTest
                 Assert.AreEqual(dataSetMetadataOld, dataSetMetadataNew);
             }
         }
+
+        [TestMethod]
+        public void TestGenerateDataSetSelectMetaData()
+        {
+            foreach (DBSyncOld.ScopeType scopeType in Enum.GetValues(typeof(DBSyncOld.ScopeType)))
+            {
+                if (scopeType == ScopeType.Historical) continue;
+
+                Tuple<string, string> dataSetMetadataOld = dbAnalyzerOld.GenerateDataSetSelectMetaData(scopeType);
+                var scopeTypeNew = (DBSyncNew.ScopeType)Enum.Parse(typeof(DBSyncNew.ScopeType), scopeType.ToString());
+                Tuple<string, string> dataSetMetadataNew = dbAnalyzerNew.GenerateDataSetSelectMetaData(scopeTypeNew);
+
+                Assert.AreEqual(dataSetMetadataOld, dataSetMetadataNew);
+            }
+        }
+
+        [TestMethod]
+        public void TestGenerateInsertMetaData()
+        {
+            foreach (DBSyncOld.ScopeType scopeType in Enum.GetValues(typeof(DBSyncOld.ScopeType)))
+            {
+                if (scopeType == ScopeType.Historical) continue;
+
+                string dataSetMetadataOld = dbAnalyzerOld.GenerateInsertMetaData(scopeType);
+                var scopeTypeNew = (DBSyncNew.ScopeType)Enum.Parse(typeof(DBSyncNew.ScopeType), scopeType.ToString());
+                string dataSetMetadataNew = dbAnalyzerNew.GenerateInsertMetaData(scopeTypeNew);
+
+                Assert.AreEqual(dataSetMetadataOld, dataSetMetadataNew);
+            }
+        }
+
+        [TestMethod]
+        public void TestGenerateSqlMetaData()
+        {
+                string dataSetMetadataOld = dbAnalyzerOld.GenerateSqlMetaData();
+                string dataSetMetadataNew = dbAnalyzerNew.GenerateSqlMetaData();
+
+                Assert.AreEqual(dataSetMetadataOld, dataSetMetadataNew);
+        }
     }
 }
