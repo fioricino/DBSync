49a0e4ffe7402a7bb20d99c320480b8b6e3860d6
fioricino <sarektoren@hotmail.com>
2017-03-26 15:14:23 +0300

    create db adapter factory

--- DBSyncNew/DBAnalyzer.cs
+++ DBSyncNew/DBAnalyzer.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Xml.Serialization;
+using DBSyncNew.Database;
 using DBSyncNew.Database.Interfaces;
 using DBSyncNew.Database.MsSql;
 using DBSyncNew.Graph;
@@ -29,6 +30,8 @@ namespace DBSyncNew
 		string scopeConfigurationSource,
             string connectionString)
         {
+            //todo move
+            DBAdapterFactory.RegisterAdapter("mssql", cs => new MsSqlDBAdapter(cs));
             Errors = new List<string>();
             this.connectionString = connectionString;
 
@@ -43,15 +46,9 @@ namespace DBSyncNew
 
         private void InitLevelInfo()
         {
-            DataTable sqlColumns;
-            using (var sqlConnection = new SqlConnection(connectionString))
-            {
-                sqlConnection.Open();
+            var dbAdapter = DBAdapterFactory.GetDBAdapter("mssql", connectionString);
 
-                sqlColumns = ReadColumnDataFromDB(sqlConnection);
-            }
-
-            IDBMetadata dbMetadata = new MsSqlMetadataAdapter(sqlColumns);
+            var dbMetadata = dbAdapter.GetDbMetadata();
 
             foreach (var dbTable in dbMetadata)
             {
@@ -153,59 +150,7 @@ namespace DBSyncNew
             }
         }
 
-        private static DataTable ReadColumnDataFromDB(SqlConnection sqlConnection)
-        {
-            DataTable mdc;
-//black magic
-            var columnCommand = new SqlCommand(@"SELECT DISTINCT 
-c.name COLUMN_NAME, 
-tp.name COLUMN_TYPE,
-COLUMNPROPERTY(c.object_id, c.name, 'charmaxlen') MAX_LENGTH,
-c.precision PRECISION,
-c.scale SCALE,
-c.is_nullable IS_NULLABLE,
-CONVERT(BIT, CASE WHEN (c.is_identity = 1 OR tp.name = 'timestamp') THEN 1 ELSE 0 END) IS_READONLY,
-CONVERT(BIT, COALESCE(ix.is_primary_key, 0)) IS_PK,
-t.name TABLE_NAME,
-tt.name REFERENCED_TABLE,
-cc.NAME REFERENCED_COLUMN,
-c.column_id ORDINAL_NUMBER
-
-FROM sys.columns c
-LEFT JOIN sys.types tp
-ON c.system_type_id = tp.system_type_id
-AND c.user_type_id = tp.user_type_id
-LEFT JOIN
-(SELECT ic.column_id, ic.object_id, is_primary_key 
-FROM sys.index_columns ic
-JOIN sys.indexes i
-ON i.index_id = ic.index_id
-AND i.object_id = ic.object_id
-AND i.is_primary_key = 1) ix
-ON c.column_id = ix.column_id
-AND c.object_id = ix.object_id
-
-LEFT JOIN sys.tables t
-ON t.object_id = c.object_id
-LEFT JOIN sys.foreign_key_columns f
-ON f.parent_object_id = c.object_id
-AND f.parent_column_id = c.column_id
-LEFT JOIN sys.tables tt
-ON f.referenced_object_id = tt.object_id
-LEFT JOIN sys.columns cc
-ON f.referenced_object_id = cc.object_id
-AND f.referenced_column_id = cc.column_id
-WHERE t.schema_id = 1
-AND t.type = 'U'
-ORDER BY TABLE_NAME, ORDINAL_NUMBER
-                                                                ", sqlConnection);
-
-
-            var columnReader = columnCommand.ExecuteReader();
-            mdc = new DataTable();
-            mdc.Load(columnReader);
-            return mdc;
-        }
+   
 
         private static DataTable ReadTableDataFromDB(SqlConnection sqlConnection)
         {
---
+++ DBSyncNew/Database/DBAdapterFactory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DBSyncNew.Database.Interfaces;
+using DBSyncNew.Database.MsSql;
+
+namespace DBSyncNew.Database
+{
+    public class DBAdapterFactory
+    {
+        private static Dictionary<string, Func<string, IDBAdapter>> adapters = new Dictionary<string, Func<string, IDBAdapter>>();
+
+        public static void RegisterAdapter(string name, Func<string, IDBAdapter> constructor)
+        {
+            if (adapters.ContainsKey(name))
+            {
+                throw new InvalidOperationException($"Adapter {name} is already registered.");
+            }
+            adapters.Add(name, constructor);
+        }
+
+        public static IDBAdapter GetDBAdapter(string name, string connectionString)
+        {
+            if (!adapters.ContainsKey(name))
+            {
+                throw new InvalidOperationException($"Adapter {name} is not registered.");
+            }
+            return adapters[name].Invoke(connectionString);
+        }
+    }
+}
